473bf9fbfff2859e8f1f4bc2985af8e74e9b2f6f
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-03 15:06:51 -0300

    Finalizando desafio em Python

---
+++ challenges/0-hello-world/hello.py
@@ -0,0 +1,9 @@
+#definição da função para printar o "hello he4rtoberfest!"
+def helloFunction():
+    print("Hello He4rtoberfest!")
+
+def main():
+    #chamada da função que printa o "hello he4rtoberfest!" dentro da função principal(main)
+    helloFunction()
+if __name__ == "__main__":
+    main()
