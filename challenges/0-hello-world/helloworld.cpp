9a70edd32f2e7d4cf35cd207012d27578dc29efe
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-03 02:46:44 -0300

    Finalizando o desafio

---
+++ challenges/0-hello-world/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//função void(sem retorno esperado) que exibe a mensagem Hello He4rtoberfest!
+void hello(){
+    cout<<"Hello He4rtoberfest !"<<endl;
+}
+
+int main(){
+    //Chamada da função dentro da main para exibir a mensagem
+    hello();
+    return 0;
+}
