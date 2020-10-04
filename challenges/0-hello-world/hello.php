2c9761c61011fc223896cd4296307c88bf71defe
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-03 21:11:07 -0300

    Finalizado desafio em PHP

---
+++ challenges/0-hello-world/hello.php
@@ -0,0 +1,10 @@
+<?php
+//Crianção da função para exibir o Hello He4rtoberfest em PHP suando o echo
+function hello(){
+    echo "Hello He4rtoberfest!";
+}
+
+//Chamada da função "hello" para exibir a mensagem
+hello();
+
+?>
